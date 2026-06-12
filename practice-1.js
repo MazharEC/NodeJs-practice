25c52b371f30fb5bdfeea669d81d3dfdb02e994f
Mazhar <mazhartuhin8291@gmail.com>
2026-06-12 21:46:56 +0600

    Updated code

--- practice-1.js
+++ practice-1.js
@@ -1,9 +1,36 @@
-// Variables
-var x = 10
-let y = 20
-const z = 50
+// // Variables
+// var x = 10
+// let y = 20
+// const z = 50
 
-console.log(x);
-console.log(z);
+// console.log(x);
+// console.log(z);
+
+// console.log(x + y);
+// console.log(x - z);
+// console.log(y * z);
+// console.log(z / y);
+
+// console.log(typeof(x));
+// console.log(typeof(y));
+// console.log(typeof(z));
+
+// let name = 'Mazhar';
+// console.log('My name is: ' + name);
+
+
+
+// // Function
+// function firstFunction(name){
+//     console.log('This is a function to call name: ' + name);
+    
+// }
+
+// firstFunction(name)
+
+function addition(a, b){
+    return a + b;
+}
+console.log(`Result: ${addition(30, 40)}`);
 
 
