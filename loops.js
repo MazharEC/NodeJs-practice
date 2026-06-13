f24e3933fe28dc3848204a637b6559100616c39c
Mazhar <mazhartuhin8291@gmail.com>
2026-06-14 00:30:20 +0600

    ndbjhg b

---
+++ loops.js
@@ -0,0 +1,25 @@
+// Loops
+
+// for(let i=1; i<=10; i++){
+//     console.log(i);
+    
+// }
+
+// let i = 1;
+// while(i<=10){
+//     console.log(i*2);
+//     i++;
+// }
+
+for (let x=1; x<=20; x++){
+    if(x%2 == 0){
+        console.log(x);
+    }
+    
+}
+
+let i = 10;
+do{
+    console.log("Mazhar");
+    i--;
+}while(i>=5)
