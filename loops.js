4ce5534173d574f2ad64718ba1188f02d383f675
Mazhar <mazhartuhin8291@gmail.com>
2026-06-15 23:35:34 +0600

    DSA

--- loops.js
+++ loops.js
@@ -1,50 +1,50 @@
 // Loops
 
-for(let i=1; i<=10; i++){
-    console.log(i);
+// for(let i=1; i<=10; i++){
+//     console.log(i);
     
-}
+// }
 
-let i = 1;
-while(i<=10){
-    console.log(i*2);
-    i++;
-}
+// let i = 1;
+// while(i<=10){
+//     console.log(i*2);
+//     i++;
+// }
 
-for (let x=1; x<=20; x++){
-    if(x%2 == 0){
-        console.log(x);
-    }
+// for (let x=1; x<=20; x++){
+//     if(x%2 == 0){
+//         console.log(x);
+//     }
     
-}
+// }
 
-let i = 10;
-do{
-    console.log("Mazhar");
-    i--;
-}while(i>=5)
+// let i = 10;
+// do{
+//     console.log("Mazhar");
+//     i--;
+// }while(i>=5)
 
 
-let fruits = ['mango', 'litchi', 'PineApple', 'blackbarry', 'graps', 'apple'];
+// let fruits = ['mango', 'litchi', 'PineApple', 'blackbarry', 'graps', 'apple'];
 
-for(let item in fruits){
-    console.log(fruits[item]);
+// for(let item in fruits){
+//     console.log(fruits[item]);
     
-}
-fruits.forEach((item, idx) => {
-    console.log(item, idx);
+// }
+// fruits.forEach((item, idx) => {
+//     console.log(item, idx);
     
-})
+// })
 
-let numbers = [1,3,6,8,29,26,25,45];
+// let numbers = [1,3,6,8,29,26,25,45];
 
-let modified = numbers.map(item => item + 5)
+// let modified = numbers.map(item => item + 5)
 
-console.log(modified);
+// console.log(modified);
 
-let findEven = numbers.filter(item => item % 2 == 0)
-console.log(findEven);
+// let findEven = numbers.filter(item => item % 2 == 0)
+// console.log(findEven);
 
 
-let getSum = numbers.reduce((total, num) => total + num, 0);
-console.log(getSum);
+// let getSum = numbers.reduce((total, num) => total + num, 0);
+// console.log(getSum);
